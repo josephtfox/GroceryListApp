8ed16223801f47caa0502b77686063332c82d6d4
josephtfox <jtfo234@g.uky.edu>
2023-07-16 23:38:48 -0500

    starting the application

---
+++ UserInterface/main_view.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtWidgets
+import sys
+import subprocess
+
+class MainView:
+    def __init__(self):
+        super().__init__()
+
+    def exit_button(self):
+        pass
+
+    def login_button(self):
+        pass
+

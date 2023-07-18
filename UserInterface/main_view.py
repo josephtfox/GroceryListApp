f55ff46be6c8c7899dead958747838e6ccf55dad
josephtfox <jtfo234@g.uky.edu>
2023-07-17 21:15:54 -0500

    starting the application

--- UserInterface/main_view.py
+++ UserInterface/main_view.py
@@ -1,14 +1,36 @@
-from PyQt5 import QtWidgets
 import sys
 import subprocess
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.uic.properties import QtWidgets
 
-class MainView:
+
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-    def exit_button(self):
-        pass
+        self.setWindowTitle("Grocery List Application")
+        self.setGeometry(100, 100, 300, 200)
 
-    def login_button(self):
-        pass
+        self.setup_buttons()
+
+    def setup_buttons(self):
+        button1 = QtWidgets.QPushButton("Open Notepad", self)
+        button1.setGeometry(50, 50, 200, 30)
+        button1.clicked.connect(self.open_notepad)
+
+        button2 = QtWidgets.QPushButton("Open Calculator", self)
+        button2.setGeometry(50, 100, 200, 30)
+        button2.clicked.connect(self.open_calculator)
+
+    def open_notepad(self):
+        subprocess.Popen("notepad.exe")
+
+    def open_calculator(self):
+        subprocess.Popen("calc.exe")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
 
